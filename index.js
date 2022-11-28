35458bb920fccc6907b2b4dfb565432620eaaa5f
fela-Supa <alvinaiterra@gmail.com>
2022-11-28 14:45:09 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1*num2;
+
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+
+let mod;
+let num3 = 19;
+let num4 = 5;
+mod = num3 % num4;
+
+let max;
+max = Math.max(10,20);
